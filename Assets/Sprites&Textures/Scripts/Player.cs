cb74915cda71e6edf477e67512172d8b1c5134dc
ColeMcdaniel <32074076+ColeMcdaniel@users.noreply.github.com>
2017-11-20 12:14:36 -0600

    adds project

---
+++ Assets/Sprites&Textures/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float maxSpeed = 300;
+	public float speed = 1000f;
+	public float jumpPower =2000f;
+
+	public bool grounded;
+
+	private Rigidbody2D rb2d;
+	private Animator anim;
+
+	void Start () 
+	{
+		rb2d = gameObject.GetComponent<Rigidbody2D>();
+		anim = gameObject.GetComponent<Animator>();
+	}
+
+	void Update () 
+	{
+
+		anim.SetBool("Grounded",grounded);
+		anim.SetFloat("Speed", Mathf.Abs (Input.GetAxis ("Horizontal")));
+
+		if (Input.GetAxis ("Horizontal") < -50f) 
+		{
+			transform.localScale = new Vector3(-390, 446, 300);
+		}
+
+		if (Input.GetAxis ("Horizontal") > 50f) 
+		{
+			transform.localScale = new Vector3(390, 446, 300);
+		}
+
+		if (Input.GetButtonDown ("Jump")) 
+		{
+			rb2d.AddForce (Vector2.up * jumpPower);
+
+		}
+
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis("Horizontal");
+
+		rb2d.AddForce((Vector2.right * speed) * h);
+
+		if (rb2d.velocity.x > maxSpeed) 
+		{
+			rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+		}
+
+		if (rb2d.velocity.x < -maxSpeed) 
+		{
+			rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+		}
+	}
+}
